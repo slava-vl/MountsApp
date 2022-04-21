c075817b4fbbb075976744153b9ebf44e9bc5682
slava_vl <slavabolotaev@gmail.com>
2022-04-21 23:15:47 +0300

    first commit

---
+++ lib/mounts_app.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class MountsApp extends StatelessWidget {
+  const MountsApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('Еще одно странное приложение)'),
+      ),
+    );
+  }
+}
---
+++ lib/splash_page.dart
@@ -0,0 +1,42 @@
+import 'package:codelab_1/mounts_app.dart';
+import 'package:flutter/material.dart';
+
+import 'main.dart';
+
+class SplashPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    Future.delayed(const Duration(seconds: 2), () {
+      Navigator.of(context).push(
+        MaterialPageRoute(
+          builder: (context) => MountsApp(),
+        ),
+      );
+    });
+
+    return Container(
+      color: mainColor,
+      child: Stack(
+        children: [
+          Align(
+            alignment: Alignment.center,
+            child: Icon(
+              Icons.terrain,
+              color: Colors.white,
+              size: 90,
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Container(
+              margin: EdgeInsets.only(bottom: 80),
+              child: CircularProgressIndicator(
+                valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
